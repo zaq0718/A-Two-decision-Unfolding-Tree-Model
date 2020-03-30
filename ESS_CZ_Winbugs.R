0980f450b1e0eee97f7ff67ebc4c0266ab2b7bf1
zaq0718 <yihenwu0718@gmail.com>
2020-03-30 23:13:17 +0200

    Update ESS_CZ_Winbugs.R

--- ESS_CZ_Winbugs.R
+++ ESS_CZ_Winbugs.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(R2WinBUGS)
 ####1. Data Management:
 rm(list=ls())
-setwd("C:\\Users\\yijhenwu\\Dropbox\\Future Research\\Response Style\\Data\\ESS")
+setwd("~/Dropbox/Future Research/Response Style/Data/ESS")
 data<-read.spss("ESS8e01_1.sav",use.value.labels =F,to.data.frame = T)
 CZ<-subset(data,data$cntry=="CZ") ##Select "CZ"
 CZ<-CZ%>%select(c(dfincac, smdfslv, sbstrec, sbprvpv, sbeqsoc, sbbsntx, sblazy, sblwcoa, uentrjb, lbenent, bennent))     ##Select 11 items for welfare-state  attitudes
@@ -23,11 +23,11 @@ inits_1<-function(){
        theta=c(rep(0,people)),eta=c(rep(0,people)))
 }
 
-setwd("C:\\Users\\yijhenwu\\Dropbox\\Future Research\\Response Style\\Data\\ESS\\Winbugs\\New Models_CZ")
+setwd("~/Dropbox/Future Research/Response Style/Data/ESS/Winbugs/New Models_CZ")
 schools.sim <- bugs(CZ_data, inits, model.file = "Model_CZ_new.txt",
                     parameters = c("alpha", "delta", "theta","tau","beta","eta","xi"),
                     n.chains = 1, n.iter = 3000,
-                    bugs.directory = "C:\\WinBUGS14\\",codaPkg=TRUE)
+                    bugs.directory = "C:/WinBUGS14/",codaPkg=TRUE)
 
 ####3. Extracted parameters from Winbugs
 output<-read.bugs(schools.sim)
